c3468937b10f698f2e2a2de07acf7eb35bac4306
Rowbaine <Rowbaine>
2025-10-15 16:24:57 +0700

    Create spring boot and add frontend file to Spring boot. I put some detail in read me txt

---
+++ src/main/java/com/example/demo/controllers/MyController.java
@@ -0,0 +1,37 @@
+package com.example.demo.controllers;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.models.Card;
+import com.example.demo.models.CardDTO;
+import com.example.demo.services.CardRepository;
+
+
+
+
+
+@RestController
+@RequestMapping("/api/review")
+@CrossOrigin 
+public class MyController {
+	@Autowired
+	private CardRepository repo;
+
+	@PostMapping
+    public Card saveReview(@RequestBody Card review) {
+        // TODO: บันทึก review ลง database
+        return repo.save(review);
+    }
+	@GetMapping
+	public String showReview() {
+		return "Hello";
+	}
+}
